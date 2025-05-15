00078743068b784d565e878d45e87e2c0e642f3e
BuildTools <unconfigured@null.spigotmc.org>
2025-05-15 19:16:08 +0200

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
 import '../css/style.css'
+
+const button = document.querySelector('.button');
+const trans = document.querySelector('.trans_cover');
+
+
+
+button.addEventListener('click', ()=> {
+    trans.classList.add('active_button')
+})
